046c284ae4e66415466df78fc239f145750801d9
Yusan Nuwarawewa <yusan.nimtharu2021@gmail.com>
2024-10-28 15:20:59 +0530

    Add Library class with book management functionality

--- src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
+++ src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
@@ -15,9 +15,11 @@ public class LibraryManagementSystem {
         Library library = new Library();
         Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565");
         Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", "9780060935467");
+        Book book3 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", " 9780747532743");
 
         library.addBook(book1);
         library.addBook(book2);
+        library.addBook(book3);
 
         System.out.println("Books in the Library:");
         library.listBooks();
